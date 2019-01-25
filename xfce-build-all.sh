604ec7e32026c9d28619667d3697b6328114ece0
Ali Ahmadi <idnux09@gmail.com>
2019-01-25 14:59:37 +0700

    update Thunar:1.8.3, update Slackbuilds, and change package tags to idn

--- xfce-build-all.sh
+++ xfce-build-all.sh
@@ -122,7 +122,7 @@ for dir in \
   # The real build starts here
   TMP=$TMP OUTPUT=$OUTPUT sh ${package}.SlackBuild || exit 1
   if [ "$INST" = "1" ]; then
-    PACKAGE=$(ls $OUTPUT/${package}-${version}-*-${build}*idnux.txz 2>/dev/null)
+    PACKAGE=$(ls $OUTPUT/${package}-${version}-*-${build}*idn.txz 2>/dev/null)
     if [ -f "$PACKAGE" ]; then
       upgradepkg --install-new --reinstall "$PACKAGE"
     else
